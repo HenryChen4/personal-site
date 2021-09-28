44175405d45d3fd9369bc2758ae4ff9708d21f9a
Henry Chen <benryben@B2O2.local>
2021-09-28 09:30:24 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const path = require("path")
+const hbs = require("hbs")
+
+const app = express()
+const publicPath = path.join(__dirname + "/public")
+const viewsPath = path.join(__dirname + "/public/views")
+const partialPath = path.join(__dirname + '/public/partials')
+
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+app.use(express.static(publicPath))
+hbs.registerPartials(partialPath)
+
+const port = 8080 || process.env.port
+
+app.get('/', (req, res)=>{
+    res.render('index')
+})
+
+app.listen(port, ()=>{
+    console.log(`listening on port: ${port}`)
+})
